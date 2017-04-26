7ac0dd9f94dd95775adfc5cf7fd33156cf02d353
Eldar Shamukhamedov <shamu1001@gmail.com>
2017-04-25 19:25:26 -0700

    Remove create-react-app example code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function render(App) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+render(App);
+
+// Hot module reloading
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
